e0d05461dbb3d22f346856d0520dff98fd4f3552
AIClubUBB <49077580+AIClubUBB@users.noreply.github.com>
2019-04-22 16:52:37 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "P11.h"
 #include "TestP11.h"
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
 struct Heap {
     int n; // Size of the heap
-    TElem v[];
+    vector<TElem> v;
 };
 int left(int i)
 {
@@ -29,27 +32,52 @@ void heapify(Heap& h, int i)
 }
 void removeMin(list<TElem>& elements, int k)
 {
+    cout<<k<<" "<<elements.size()<<endl;
     if(k<=0)
     {
         exception e;
         throw e;
     }
+    if(k >= elements.size())
+    {
+        list<TElem> aux;
+        elements = aux;
+        return;
+    }
     Heap h;
     h.n=elements.size();
-    h.v[h.n];
+    //cout<<h.n<<endl;
+    int k_aux = k;
+    //copy(elements.begin(), elements.end(), h.v);
     int index=0;
-    for (list<int>::iterator it=elements.begin(); it != elements.end(); it++,index++)
+    for(auto it=elements.begin();it!=elements.end();it++,index++)
     {
-        h.v[index]=*it;
+        h.v.push_back(*it);
+        //cout<<h.v[h.v.size()-1]<<endl;
+        //int val=*it;
+        //cout<<index<<" "<<*it<<endl;
+        //h.v[index]=val;
     }
+    //cout << h.v[h.n - 1] << endl;
+    index=0;
     int pozStart=0;
+
     while(k!=0)
     {
+        cout<<pozStart<< " "<<k<<endl;
         heapify(h,pozStart);
         k/=2;
         pozStart+=k;
     }
-
+    while(!elements.empty())
+    {
+        elements.pop_back();
+    }
+    cout << elements.size();
+    for(int i=k_aux;i<h.v.size();i++)
+    {
+        elements.push_back(h.v[i]);
+    }
 }
 
 int main()
